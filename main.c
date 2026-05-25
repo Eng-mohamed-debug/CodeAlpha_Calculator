0e5d24d18d610f7f8726613f15d5575e50d565b6
Eng-mohamed-debug <mohamedabdo.business2025@gmail.com>
2026-05-26 00:57:08 +0300

    finish the simple calculator task and handle zero division case and test all possible cases

--- main.c
+++ main.c
@@ -3,8 +3,45 @@
 
 int main(void)
 {
+    double num1, num2, result;  
+    char operation;
+
+    printf("Enter the First Number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the second Number: ");
+    scanf("%lf", &num2);
+
+    printf("Insert the Operation you want (+, -, /, *): ");
+    scanf(" %c", &operation);
     
+    switch (operation)
+    {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        if(num2 == 0)
+        {
+            printf("You can't divide by zero.\n");
+            return 1; // to indicate the error
+        }
+        else
+        {
+            result = num1 / num2;
+        }
+        break;
+    default:
+        printf("Enter a valid option");
+        break;
+    }
 
-
+    printf("%.2lf %c %.2lf = %.2lf", num1, operation, num2, result);
     return 0;
 }
